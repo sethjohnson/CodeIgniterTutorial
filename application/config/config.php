6645a2b6380637b7394381ba8176c99436e2812e
Seth Johnson <seth@appfirst.com>
2012-08-31 09:51:44 -0500

    reset base_url path

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://sethscrypt.dyndns.org';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
