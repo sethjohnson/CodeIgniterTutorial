458ac3499a9a3806bef86a4b486eb7639a271052
Seth Johnson <sethalanjohnson@gmail.com>
2013-02-14 17:02:03 -0600

    Done a lot of work, includes summer plan

--- application/views/pages/payment.php
+++ application/views/pages/payment.php
@@ -1,3 +1,37 @@
+<?php
+$months = array(
+  "September",
+  "October",
+  "November",
+  "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August"
+  );
+$dudes = array(
+"Jared Grove" => 9,
+  "Jeffy Hong-dawg" => 4,
+  "Nirmal Mekala" => 6,
+  "Will Moon" => 3,
+  "Matt Reynertson" => 6,
+  "Reed Schaaf" => 7,
+  "Nathan \"Cobra\" Tripp" => 4,
+  "Scooter Waltinator" => 3);
+
+  $total_months = 12;
+  $month = 5;
+  $last_single_month = 2;
+  $last_compound_month = 8
+?>
+
+
+
+
 <h3>Order Quantity</h3>
 <p>The awesome Internet payments can be made monthly, or once for the entire year. You may notice that you will save about 2 dollars by paying for all your internet up-front: This is because there is a small PayPal Service charge (less than a dollar) per transaction. There is no problem at all with paying monthly; I have simply added the fee to the payment price.</p>
 <h3>What You're Paying For</h3>
@@ -8,138 +42,123 @@
     <li>Monthly Internet Subscription (Around $45) </li>
 </ul>
 <p> Once again, I've rounded up by a few cents for each transaction to cover various expenses including PayPal transaction fees and ComCast payment service fees. If you have any questions or concerns regarding these payments, do not hesitate to email me at <a href="mailto:sethalanjohnson@gmail.com">sethalanjohnson@gmail.com</a>!</p>
-
-<h2>Payment for the months of September & October, 2012</h2>
+<?php
+  echo "<h2>Payment for the month of " . $months[$month-1] . ", 2012</h2>";
+?>
+<p>Note: To avoid any complications Summer may bring with people being gone, I've split everyones fees for June, July, and August between the months of December - May. This way, no one will owe me any money for internet during the three Summer months. If you have a subleaser, you can certainly ask them to pay <em>you</em> for the Summer internet, but as of May all debts for the year will be paid. If you have any questions about this complication, let me know!</p>  
+<!--
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="J6YVFVGQ4GY2A">
 <table>
-<tr><td><input type="hidden" name="on0" value="Quantity">Quantity</td></tr><tr><td><select name="os0">
-<option value="Monthly Internet Payment">Monthly Internet Payment $7.25 USD</option>
-<option value="Full-Year Internet Payment">Full-Year Internet Payment $85.00 USD</option>
+<tr><td><input type="hidden" name="on0" value="Number of Payments">Number of Payments</td></tr><tr><td><select name="os0">
+	       <option value="1 Month">1 Month $7.25 USD</option>
+	       <option value="2 Months">2 Months $14.50 USD</option>
+	       <option value="3 Months">3 Months $21.75 USD</option>
+	       <option value="4 Months">4 Months $29.00 USD</option>
+	       <option value="5 Months">5 Months $36.25 USD</option>
+	       <option value="6 Months">6 Months $43.50 USD</option>
+	       <option value="7 Months">7 Months $50.75 USD</option>
+	       <option value="8 Months">8 Months $58.00 USD</option>
+	       <option value="9 Months">9 Months $65.25 USD</option>
+	       <option value="10 Months">10 Months $72.50 USD</option>
 </select> </td></tr>
-<tr><td><input type="hidden" name="on1" value="Your Full Name">Your Full Name</td></tr><tr><td><input type="text" name="os1" maxlength="200"></td></tr>
+<tr><td><input type="hidden" name="on1" value="Your Name">Your Name</td></tr><tr><td><select name="os1">
+	       <option value="Jared Grove">Jared Grove </option>
+	       <option value="Jeff Hong">Jeff Hong </option>
+	       <option value="Nirmal Mekala">Nirmal Mekala </option>
+	       <option value="Will Moon">Will Moon </option>
+	       <option value="Matt Reynertson">Matt Reynertson </option>
+	       <option value="Reed Schaaf">Reed Schaaf </option>
+	       <option value="Nathan Tripp">Nathan Tripp </option>
+	       <option value="Scott Walter">Scott Walter </option>
+</select> </td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIH8QYJKoZIhvcNAQcEoIIH4jCCB94CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBlOf9TSjxzGPvghQWvtFQTi2RsmH6d6X6lp3pDfzMN2Ks6iiyLjOIOUhxjC9BtRaCZd39WGRN8uz0pbfxgZH6ZSTy8EvqC9YfNSn6HMWqjBlMqineX7o8odeGyJt5e9R+qDLCLIvLMhXGD7GrSOahU0w2ouLIoOuQp2yw1upkfGDELMAkGBSsOAwIaBQAwggFtBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECK3zdeCyTP9hgIIBSADGkzWwhfXQ4xzQT5+bw6BLUYog3B0pPLvYKu1VS9ixr1ai72DLz2OhFOn2g3eihSGTE4ADC/A6WKuOJsHMalAkHL2XYLos8CDEgQAzfZha3Ig8pHyM/rsL+DBhgJQMNRU1W/PnJYSPDwzTS1HnCOW7ZKdR11ZheMT46QQR3CoAulW35nBAbDeqfuNGTdCY6ADBOk1ER0BnjujnBAzQIFXpXtRD28678Dsd91aT+YqkhV446sIQtKtKzYuXrrSF075NPwbyNlMabW5nbu8RmLWZJ1MLhQ9ilBh9SpLMuQpM1OH+yGsePjJO9WJ9vKkaqyHS7uIJk0Ldf3Sz/lll3hokDaT0H4HZGajWbgFEIjW+XLCE3w8D1wKoUorf/m/ncOdajS8yYqM3PEfLDU5LbEjCkFwB3mT78JezmyMJTkc7/Kfc9XMtiySgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMjA5MTkxODI4MTNaMCMGCSqGSIb3DQEJBDEWBBTksj8nVvFTX2/jE1Oqm6u+8sLVlTANBgkqhkiG9w0BAQEFAASBgEe0JC/DDOCMHRRr1gpNb2K2bPh8Lg6cutSzR/36j5dIh3HORIBL7ZC7cHebkEgWnH/5npWM0MOIgnJw1dDkKiCJaEAPTn7/xyoT6yNr6/OgSS4srzW2Q2G6B8m+Mjb7Ulgl/oJ43M8cqQvR+yk1c2FBESFM5ZATFOQvrqk0KUBY-----END PKCS7-----
-">
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+--!>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="JFF5NYJVTKDKW">
+<table>
+<tr><td><input type="hidden" name="on0" value="Number of Payments">Number of Payments</td></tr><tr><td><select name="os0">
+	       <option value="1 Month">1 Month $10.88 USD</option>
+	       <option value="2 Months">2 Months $21.75 USD</option>
+	       <option value="3 Months">3 Months $32.63 USD</option>
+	       <option value="4 Months">4 Months $43.50 USD</option>
+	       <option value="5 Months">5 Months $54.38 USD</option>
+	       <option value="6 Months">6 Months $65.25 USD</option>
+</select> </td></tr>
+<tr><td><input type="hidden" name="on1" value="Your Name">Your Name</td></tr><tr><td><select name="os1">
+	       <option value="Jared Grove">Jared Grove </option>
+	       <option value="Jeff Hong">Jeff Hong </option>
+	       <option value="Nirmal Mekala">Nirmal Mekala </option>
+	       <option value="Will Moon">Will Moon </option>
+	       <option value="Matt Reynertson">Matt Reynertson </option>
+	       <option value="Reed Schaaf">Reed Schaaf </option>
+	       <option value="Nathan Tripp">Nathan Tripp </option>
+	       <option value="Scott Walter">Scott Walter </option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
 
 
 
+
+
+
+
+
 <table border="1">
 <tr>
-  <th/>
-  <th>September</th>
-  <th>October</th>
-  <th>November</th>
-  <th>December</th>
-  <th>January</th>
-  <th>February</th>
-  <th>March</th>
-  <th>April</th>
-  <th>May</th>
-</tr>
-<tr>
-  <th>Jared Grove</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
+  <th style='width:350px'/>
+  <?php 
+  for ($i=0; $i < $total_months; $i++) { 
+    if ($i != $month-1) {
+       if ($i <= $last_compound_month) {
+       	  echo "<th style='width:100px;'>";
+       } else {
+       	  echo "<th style='width:100px;background-color:LightGrey'>";
+       }
+    } else {
+     echo "<th style='width:100px;background-color:OrangeRed'>";
+
+    }
+    echo $months[$i] . "</th>"; 
+  }
+  ?>
 </tr>
 
-<tr>
-  <th>Jeff Hong</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
+<?php 
+  foreach ($dudes as $name => $payments) {
+    echo "<tr>";
+    echo "<th align='right'>" . $name . "</th>";
+    for ($i=1; $i <= $total_months; $i++) { 
+      if ($i <= $payments || ($i > $last_compound_month+1 && $i - ($last_compound_month+1) <= ($payments-$last_single_month-1)/2) ) {
+        //Been paid for
+        echo "<td align='center' style='background-color:GreenYellow'>PAID</td>";
 
-<tr>
-  <th>Nirmal Mekala</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
-
-<tr>
-  <th>Will Moon</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
-<tr>
-  <th>Matt Reynertson</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
-<tr>
-  <th>Reed Schaff</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
-<tr>
-  <th>Nathan Tripp</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
-<tr>
-  <th>Scott Walter</th>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-  <td style="background-color:GreenYellow"/>
-</tr>
+      } else {
+        //Has not been paid for
+        if ($i <= $month) {
+          //Due
+          echo "<td align='center' style='background-color:OrangeRed'>DUE!</td>";
+        } else if ( $i <= $last_compound_month+1 ) {
+          //not yet due
+          echo "<td style='background-color:White'/>";
+        } else {
+	  echo "<td style='background-color:LightGrey'/>";
+	}
+      } 
+    }
+  }
+  ?>
 
 </table>
