886f10e99741e7b2d92eb0fa9d4c1e2e07188bb7
Seth Johnson <sethalanjohnson@gmail.com>
2013-10-02 16:27:43 -0500

    threw in an old assignment

--- index.php
+++ index.php
@@ -1,8 +1,91 @@
-<html>
+<!DOCTYPE html>
+
+<html >
 <head>
-<title>MY PAGE</title>
+	<meta charset = "utf-8">
+	<link rel="stylesheet" type="text/css" href="mycsstyle.css">
+	<title>Title</title>
 </head>
 <body>
-<p> It's working! IT'S WORKING! </p>
+	<h1>My Fall 2013 Class Schedule</h1>
+	<table >
+		<thead>
+			<tr>
+				<th>&nbsp;</th>
+				<th>Monday</th>
+				<th>Tuesday</th>
+				<th>Wednesday</th>
+				<th>Thursday</th>
+				<th>Friday</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>8:15 - 9:30</th>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>9:45 - 11:00</th>
+				<td><a target = "_blank" href="http://www-users.cselabs.umn.edu/classes/Fall-2013/csci5115/">CSCI 5115</a></td>
+				<td>&nbsp;</td>
+				<td><a target = "_blank" href="http://www-users.cselabs.umn.edu/classes/Fall-2013/csci5115/">CSCI 5115</a></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>11:15- 12:30</th>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>1:00 - 2:15</th>
+				<td>&nbsp;</td>
+				<td><a target = "_blank" href="http://www-users.cselabs.umn.edu/classes/Fall-2013/csci5980/">CSCI 5980</a></td>
+				<td>&nbsp;</td>
+				<td><a target = "_blank" href="http://www-users.cselabs.umn.edu/classes/Fall-2013/csci5980/">CSCI 5980</a></td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>2:30 - 3:45</th>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>4:00 - 5:15</th>
+				<td><a target = "_blank" href="https://ay13.moodle.umn.edu/course/view.php?id=6328">CSCI 5523</a></td>
+				<td>&nbsp;</td>
+				<td><a target = "_blank" href="https://ay13.moodle.umn.edu/course/view.php?id=6328">CSCI 5523</a></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>6:30 - 9:00</th>
+				<td>&nbsp;</td>
+				<td><a target = "_blank" href="http://www-users.cselabs.umn.edu/classes/Fall-2013/csci4131/">CSCI 4131</a></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>			
+
+		</tbody>
+
+	</table>
+
 </body>
 </html>
