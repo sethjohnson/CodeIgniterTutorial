5fd0a4c29355c5d9ad234d8c4d075b5830693e04
Seth Johnson <sethalanjohnson@gmail.com>
2013-10-02 16:29:18 -0500

    threw in an old assignment css file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html >
 <head>
 	<meta charset = "utf-8">
-	<link rel="stylesheet" type="text/css" href="mycsstyle.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Title</title>
 </head>
 <body>
